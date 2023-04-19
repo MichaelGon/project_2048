8e788323abdf5f327778cfa84b763f0cee2a1af3
MichaelGon <45266469+MichaelGon@users.noreply.github.com>
2023-04-19 13:17:47 +0300

    Update game_loop.py

--- game_loop.py
+++ game_loop.py
@@ -1,52 +1,54 @@
 from func import is_any, make_turn, new_pieces
+from init import Globals
 import pygame
 import sqlite3
 from visual import draw_over, draw_board, draw_pieces
 
 
-def loop(game):
+class Loop(object):
     is_game = True
+    game = Globals()
 
-    while is_game:
-        game.timer.tick(game.fps)
-        game.screen.fill('gray')
-        draw_board(game)
-        draw_pieces(game)
+    def __init__(self, elem):
+        self.game = elem
 
-        if game.som_new or game.initer < 2:
-            game.board_values, game.game_over = new_pieces(game)
-            game.som_new = False
-            game.initer += 1
+    def upload(self):
+        with sqlite3.connect("2048.sqlite") as bd:
+            cur = bd.cursor()
 
-        if game.direction != '':
-            game.board_values = make_turn(game)
-            game.direction = ''
-            game.som_new = True
+            cur.execute("""
+            CREATE TABLE IF NOT EXISTS RECORDS (
+                name text,
+                score integer
+            )""")
 
-        if game.game_over and not is_any(game):
-            draw_over(game)
-            if game.high_score > game.init_high:
-                file = open('high_score', 'w')
-                file.write(f'{game.high_score}')
-                file.close()
-                game.init_high = game.high_score
+            cur.execute("""
+            SELECT name gamer, max(score) score
+            FROM RECORDS
+            GROUP BY name
+            ORDER BY score DESC
+            limit 3
+            """)
 
+            self.game.REZ = cur.fetchall()
+
+    def event(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                is_game = False
+                self.is_game = False
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_UP:
-                    game.direction = 'UP'
+                    self.game.direction = 'UP'
                 elif event.key == pygame.K_DOWN:
-                    game.direction = 'DOWN'
+                    self.game.direction = 'DOWN'
                 elif event.key == pygame.K_LEFT:
-                    game.direction = 'LEFT'
+                    self.game.direction = 'LEFT'
                 elif event.key == pygame.K_RIGHT:
-                    game.direction = 'RIGHT'
+                    self.game.direction = 'RIGHT'
 
-                if game.game_over and not is_any(game):
+                if self.game.game_over and not is_any(self.game):
                     ans = list()
-                    helper = (game.USERNAME, game.score)
+                    helper = (self.game.USERNAME, self.game.score)
                     ans.append(helper)
 
                     with sqlite3.connect("2048.sqlite") as bd:
@@ -56,33 +58,43 @@ def loop(game):
 
                         cur.close()
                     if event.key == pygame.K_RETURN:
-                        game.board_values = [[0 for _ in range(4)] for _ in range(4)]
-                        game.som_new = True
-                        game.initer = 0
-                        game.score = 0
-                        game.direction = ''
-                        game.game_over = False
+                        self.game.board_values = [[0 for _ in range(4)] for _ in range(4)]
+                        self.game.som_new = True
+                        self.game.initer = 0
+                        self.game.score = 0
+                        self.game.direction = ''
+                        self.game.game_over = False
 
-                    with sqlite3.connect("2048.sqlite") as bd:
-                        cur = bd.cursor()
+                    self.upload()
 
-                        cur.execute("""
-                        CREATE TABLE IF NOT EXISTS RECORDS (
-                            name text,
-                            score integer
-                        )""")
+    def loop(self):
+        while self.is_game:
+            self.game.timer.tick(self.game.fps)
+            self.game.screen.fill('gray')
+            draw_board(self.game)
+            draw_pieces(self.game)
 
-                        cur.execute("""
-                        SELECT name gamer, max(score) score
-                        FROM RECORDS
-                        GROUP BY name
-                        ORDER BY score DESC
-                        limit 3
-                        """)
+            if self.game.som_new or self.game.initer < 2:
+                self.game.board_values, self.game.game_over = new_pieces(self.game)
+                self.game.som_new = False
+                self.game.initer += 1
 
-                        game.REZ = cur.fetchall()
+            if self.game.direction != '':
+                self.game.board_values = make_turn(self.game)
+                self.game.direction = ''
+                self.game.som_new = True
 
-        if game.score > game.high_score:
-            game.high_score = game.score
+            if self.game.game_over and not is_any(self.game):
+                draw_over(self.game)
+                if self.game.high_score > self.game.init_high:
+                    file = open('high_score', 'w')
+                    file.write(f'{self.game.high_score}')
+                    file.close()
+                    self.game.init_high = self.game.high_score
 
-        pygame.display.flip()
+            self.event()
+
+            if self.game.score > self.game.high_score:
+                self.game.high_score = self.game.score
+
+            pygame.display.flip()
